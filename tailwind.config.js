5b18d6e8a33b33b4aff4058940ec1b8642f2cfce
Josh Hamilton <joshhamilton@MacBook-Air-3.local>
2025-04-06 18:05:19 +0200

    Clean deploy

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        border: '#e5e7eb',
+      },
+    },
+  },
+  plugins: [],
+}

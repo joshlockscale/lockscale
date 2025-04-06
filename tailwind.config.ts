26b0ecd4673bb2933b228e9ccfd2bba2dd1c35fa
Josh Hamilton <joshhamilton@MacBook-Air-3.local>
2025-04-06 18:12:58 +0200

    Fix: properly define border color in Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,10 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {
       colors: {
@@ -8,10 +12,6 @@ const config: Config = {
       },
     },
   },
-  content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
   plugins: [],
 };
 

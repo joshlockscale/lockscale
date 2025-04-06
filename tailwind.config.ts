819687b54f0dbf8da5f08548d5cd42168b13ba16
Josh Hamilton <joshhamilton@MacBook-Air-3.local>
2025-04-06 18:11:09 +0200

    Fix Tailwind config for border-border

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,18 @@
-export default {
-    theme: {
-      extend: {
-        colors: {
-          border: 'hsl(240, 4.8%, 83.9%)',
-        },
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  theme: {
+    extend: {
+      colors: {
+        border: 'hsl(240, 4.8%, 83.9%)',
       },
     },
-  };  
+  },
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  plugins: [],
+};
+
+export default config;
